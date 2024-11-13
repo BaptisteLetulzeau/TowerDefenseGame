d739afdf535caf20eb90c042d16554960166426f
BaptisteLetulzeau <baptiste.letulzeau@elloha.com>
2024-11-13 10:34:57 +0100

    Revert "commit"
    This reverts commit 9be9beb280c81965443af5d060cccb1141b0e5f2.

--- src/entities/enemies/Knight.java
+++ src/entities/enemies/Knight.java
@@ -5,9 +5,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.image.Image;
-import javafx.scene.image.WritableImage;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import java.util.List;
 
@@ -23,72 +20,50 @@ public class Knight extends Enemies {
     private Timeline animation;
     private List<Point2D> waypoints;
     private int currentWaypointIndex = 0;
-    private double speed = 1;
+    private double speed = 2.0;
     private static final int centerY = 500;
-    private static final double DISTANCE_THRESHOLD = 5.0;
-    private Image spriteSheet;
 
     public Knight(List<Point2D> waypoints) {
         super("assets/images/enemies/Warrior.png", waypoints);
         this.waypoints = waypoints;
-        this.spriteSheet = new Image("assets/images/enemies/Warrior.png");
 
-        setImage(subImage(spriteSheet, 0, FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT));
-        setLayoutX(0);
-        setLayoutY(centerY);
-
-        System.out.println("Image width: " + spriteSheet.getWidth());
-        System.out.println("Image height: " + spriteSheet.getHeight());
+        setX(0);
+        setY(centerY);
 
         setFitWidth(FRAME_WIDTH);
         setFitHeight(FRAME_HEIGHT);
-        //setViewport(new Rectangle2D(0, FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT));
+        setViewport(new Rectangle2D(0, 0, FRAME_WIDTH, FRAME_HEIGHT));
 
         startAnimation();
     }
 
     private void startAnimation() {
         animation = new Timeline(new KeyFrame(Duration.millis(100), event -> {
-            move();
             updateFrame();
+            move();
         }));
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
     }
 
-    public void move() {
-        if (hasReachedFinalWaypoint()) {
-            stopAnimation();
-            System.out.println("Knight a atteint le point final.");
-            return;
-        }
-
-        double newX = getLayoutX() + speed;
-        setLayoutX(newX);
-        setLayoutY(centerY);
-        System.out.println(newX);
+    private void move() {
+        double newX = getX() + speed;
+        setX(newX);
+        setY(centerY);
     }
 
     private void updateFrame() {
         int col = currentFrame % COLUMNS;
         int row = 1;
+        System.out.println(currentFrame);
+
+        if (currentFrame >= FRAMES_IN_SECOND_LINE) {
+            currentFrame = 0;
+        }
 
         setViewport(new Rectangle2D(col * FRAME_WIDTH, row * FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT));
 
         currentFrame = (currentFrame + 1) % FRAMES_IN_SECOND_LINE;
-    }
-
-    private Image subImage(Image source, int x, int y, int width, int height) {
-        WritableImage image = new WritableImage(width, height);
-        image.getPixelWriter().setPixels(0, 0, width, height, source.getPixelReader(), x, y);
-        return image;
-    }
-
-    private boolean hasReachedFinalWaypoint() {
-        Point2D finalWaypoint = waypoints.getLast();
-        double distanceToFinalWaypoint = finalWaypoint.distance(getLayoutX(), getLayoutY());
-
-        return distanceToFinalWaypoint <= DISTANCE_THRESHOLD;
     }
 
     public void stopAnimation() {
--- src/game/GameScene.java
+++ src/game/GameScene.java
@@ -4,8 +4,6 @@ import controllers.GameController;
 import entities.towers.ArrowTower;
 import entities.towers.FlailMan;
 import entities.towers.Towers;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
@@ -16,7 +14,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +25,6 @@ public class GameScene {
     private final VBox rootLayout;
     private final List<Rectangle> towerSpots;
     private final GameController gameController;
-    private Timeline gameLoop;
 
     public GameScene() {
         gamePane = new Pane();
@@ -41,21 +37,7 @@ public class GameScene {
         setupMouseClickListener();
 
         rootLayout = new VBox(towerTypeComboBox, gamePane);
-        gameController = new GameController(gamePane);
-
-        setupGameLoop();
-    }
-
-    private void update() {
-        gameController.update();
-    }
-
-    private void setupGameLoop() {
-        gameLoop = new Timeline(new KeyFrame(Duration.millis(16), e -> {
-            update();
-        }));
-        gameLoop.setCycleCount(Timeline.INDEFINITE);
-        gameLoop.play();
+        gameController = new GameController(rootLayout);
     }
 
     public VBox getRootLayout() {
